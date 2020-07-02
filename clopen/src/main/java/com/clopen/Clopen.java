1cfb2eca07ac27f4c1a8efd2c1909a7f23327ebf
William Wyatt <tsangares@gmail.com>
2020-07-02 14:36:22 -0700

    Changed it to pairs

--- clopen/src/main/java/com/clopen/Clopen.java
+++ clopen/src/main/java/com/clopen/Clopen.java
@@ -1,23 +1,25 @@
 package com.clopen;
+import java.util.Arrays
 import java.util.ArrayList;
 import java.awt.Point;
+import javafx.util.Pair;
 
 class Clopen{
     public static void main(String args[]){
 	
     }
 
-    // Given a list of points which form edges
+    // Given a list of edges
     // Return True if list of point arr is connected and forms a loop
     // Return Flase if arr is either disconnected or does not form a loop
-    public static boolean isClosed(ArrayList<Point> arr){
+    public static boolean isClosed(ArrayList<Pair<Point,Point>> arr){
 	return false;
     }
 
-    // Given a list of points which form an enclosure and a point p
+    // Given a list of edges
     // Return True if the point p is inside the enclosure
     // Return False if the point p is outside the enclosure
-    public static boolean isInside(ArrayList<Point> arr, Point p){
+    public static boolean isInside(ArrayList<Pair<Point,Point>> arr, Point p){
 	if (!isClosed(arr)) return false;
 	//Test if inside closed
 	return false;
@@ -25,7 +27,7 @@ class Clopen{
 
     // Given a list of points which form edges
     // Return a list of all lists that form enclosurers (pass isClosed)
-    public static ArrayList<ArrayList<Point>> getEnclosures(ArrayList<Point> arr){
+    public static ArrayList<ArrayList<Point>> getEnclosures(ArrayList<Pair<Point,Point>> arr){
 	ArrayList<ArrayList<Point>> enclosurers = new ArrayList<ArrayList<Point>>();
 	//Find enclosures given a list of walls in arr.
 	return enclosurers;
