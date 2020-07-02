182425a06b5987d3c3aad4faba6f58bc3aedc1c4
William Wyatt <tsangares@gmail.com>
2020-07-02 15:04:56 -0700

    Added edge class

--- clopen/src/main/java/com/clopen/Clopen.java
+++ clopen/src/main/java/com/clopen/Clopen.java
@@ -1,8 +1,7 @@
 package com.clopen;
-import java.util.Arrays
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.awt.Point;
-import javafx.util.Pair;
 
 class Clopen{
     public static void main(String args[]){
@@ -12,14 +11,14 @@ class Clopen{
     // Given a list of edges
     // Return True if list of point arr is connected and forms a loop
     // Return Flase if arr is either disconnected or does not form a loop
-    public static boolean isClosed(ArrayList<Pair<Point,Point>> arr){
+    public static boolean isClosed(ArrayList<Edge> arr){
 	return false;
     }
 
     // Given a list of edges
     // Return True if the point p is inside the enclosure
     // Return False if the point p is outside the enclosure
-    public static boolean isInside(ArrayList<Pair<Point,Point>> arr, Point p){
+    public static boolean isInside(ArrayList<Edge> arr, Point p){
 	if (!isClosed(arr)) return false;
 	//Test if inside closed
 	return false;
@@ -27,8 +26,8 @@ class Clopen{
 
     // Given a list of points which form edges
     // Return a list of all lists that form enclosurers (pass isClosed)
-    public static ArrayList<ArrayList<Point>> getEnclosures(ArrayList<Pair<Point,Point>> arr){
-	ArrayList<ArrayList<Point>> enclosurers = new ArrayList<ArrayList<Point>>();
+    public static ArrayList<ArrayList<Edge>> getEnclosures(ArrayList<Edge> arr){
+	ArrayList<ArrayList<Edge>> enclosurers = new ArrayList<ArrayList<Edge>>();
 	//Find enclosures given a list of walls in arr.
 	return enclosurers;
     }
---
+++ clopen/src/main/java/com/clopen/Edge.java
@@ -0,0 +1,15 @@
+package com.clopen;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.awt.Point;
+
+
+public class Edge{
+    public Point alpha = null;
+    public Point beta = null;
+    
+    public Edge(Point a, Point b){
+	alpha = a;
+	beta = b;
+    }
+}
