ca017ad6f926dab48f9034301fbb578b425c6d3f
William Wyatt <tsangares@gmail.com>
2020-07-30 11:48:43 -0700

    Rust attempt.

--- src/main/java/com/clopen/Clopen.java
+++ src/main/java/com/clopen/Clopen.java
@@ -3,15 +3,24 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.awt.Point;
 
+import org.wasmer.Instance;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 class Clopen{
-    public static void main(String args[]){
-	
+    public static void main(String args[]) throws IOException{
+	System.out.println("Initialized.");
+	byte[] bytes = Files.readAllBytes(Paths.get("src/main/java/com/clopen/rust_bg.wasm"));
+	Instance instance = new Instance(bytes);
+	instance.exports.getFunction("world").apply();
     }
 
     // Given a list of edges
     // Return True if list of point arr is connected and forms a loop
     // Return Flase if arr is either disconnected or does not form a loop
     public static boolean isClosed(ArrayList<Edge> arr){
+	
 	return false;
     }
 
---
+++ src/main/rust/src/lib.rs
@@ -0,0 +1,6 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub extern fn world(){
+    println!("Hello World");
+}
